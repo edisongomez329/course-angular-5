dde6a93a3924bbf40cc6d66718d62d38e1224046
Edison Gómez Martinez <edisondev@outlook.com>
2018-03-25 23:04:38 -0500

    Modificaciones en modulos y rutas, se simplifican en el appModule  para cuando el proyecto  vaya creciendo demaciado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,11 @@
+
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
-
 const appRoutes: Routes = [
-    {
-        path: '', component: PagesComponent,
-        children: [
-            {path: 'dashboard', component: DashboardComponent},
-            {path: 'progress', component: ProgressComponent},
-            {path: 'graficas1', component: Graficas1Component},
-            // Cuando la ruta queda en limpio se redirige al dashboard
-            {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-        ]
-    },
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     // Devuelve al component no page found cuando se escribe algo que pertenece a la ruta
